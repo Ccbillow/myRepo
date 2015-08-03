386b6729c4d6ec40129d4441ebfccad370a122a5
tao.cheng <ctaoaoo@iCloud.com>
2015-08-03 14:17:45 +0800

    update

--- src/main/java/org/cbillow/magic/GetHubJavaProcesser.java
+++ src/main/java/org/cbillow/magic/GetHubJavaProcesser.java
@@ -11,7 +11,7 @@ import us.codecraft.webmagic.processor.PageProcessor;
 public class GetHubJavaProcesser implements PageProcessor{
 
 	private Site site = Site.me().setRetryTimes(3).setSleepTime(1000) ;
-	
+
 	public void process(Page page) {
 		List<String> urls = page.getHtml().css("div.pagination").links().all();
 		page.addTargetRequests(urls);
@@ -20,11 +20,11 @@ public class GetHubJavaProcesser implements PageProcessor{
 	public Site getSite() {
 		return site;
 	}
-	
+
 	public static void main(String[] args) {
 		Spider.create(new GetHubJavaProcesser())
 			.addUrl("https://github.com/search?l=Java&p=1&q=stars%3A%3E1&s=stars&type=Repositories")
-			.addPipeline(new ConsolePipeline()).run(); 
+			.addPipeline(new ConsolePipeline()).run();
 	}
-	
+
 }
