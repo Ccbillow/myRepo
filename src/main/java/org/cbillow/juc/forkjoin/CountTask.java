b4b6c2de97d76d78b68b7bf4bf6e67fdb8371949
tao.cheng <ctaoaoo@iCloud.com>
2015-10-08 14:47:53 +0800

    update

---
+++ src/main/java/org/cbillow/juc/forkjoin/CountTask.java
@@ -0,0 +1,66 @@
+package org.cbillow.juc.forkjoin;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinTask;
+import java.util.concurrent.RecursiveTask;
+
+/**
+ * Fork/Join实例
+ * 计算1+2+3+4
+ */
+public class CountTask extends RecursiveTask<Integer> {
+
+    private static final int THRESHOLD = 2;
+    private int start;
+    private int end;
+
+    public CountTask(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    protected Integer compute() {
+        int sum =0;
+
+        //如果任务足够小就计算任务
+        boolean canCompute = (end - start) <= THRESHOLD;
+        if (canCompute) {
+            for (int i = start; i <= end; i++) {
+                sum += i;
+            }
+        } else {
+            //如果大于阀值，就分裂成两个子任务计算
+            int middle = (start + end) / 2;
+            CountTask leftTask = new CountTask(start, middle);
+            CountTask rightTask = new CountTask(middle + 1, end);
+
+            //执行子任务
+            leftTask.fork();
+            rightTask.fork();
+
+            Integer leftResult = leftTask.join();
+            Integer rightResult = rightTask.join();
+
+            //合并子任务
+            sum = leftResult + rightResult;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        ForkJoinPool pool = new ForkJoinPool();
+        //生成一个计算任务，计算1+2+3+4
+        CountTask task = new CountTask(1, 4);
+        //执行一个任务
+        ForkJoinTask<Integer> result = pool.submit(task);
+        try {
+            System.out.println(result.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+}
