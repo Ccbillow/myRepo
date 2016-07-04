6e3240b405f43b066232d1acf83d9ff882a65309
tao.cheng <ctaoaoo@iCloud.com>
2016-07-04 22:20:19 +0800

    lombox

---
+++ src/main/java/org/cbillow/lombok/LomboxTest.java
@@ -0,0 +1,17 @@
+package org.cbillow.lombok;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author Created by Cbillow
+ * @date 16/7/4
+ * @time 22:07
+ */
+public class LomboxTest {
+
+    @Getter @Setter private String name;
+    @Getter @Setter private String password;
+
+
+}
---
+++ src/main/java/org/cbillow/lombok/Test.java
@@ -0,0 +1,15 @@
+package org.cbillow.lombok;
+
+/**
+ * @author Created by Cbillow
+ * @date 16/7/4
+ * @time 22:08
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        LomboxTest lomboxTest = new LomboxTest();
+        lomboxTest.setName("dsds");
+        System.out.println(lomboxTest.getName());
+    }
+}
